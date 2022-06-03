8556ad44288392296078f60d22904036e3e2a448
student <i.i.f@mail.ru>
2022-06-04 01:10:08 +0800

    all tasks

---
+++ src/main/java/pro/sky/shop/service/ShoppingCartService.java
@@ -0,0 +1,37 @@
+package pro.sky.shop.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+import pro.sky.shop.data.DBGoods;
+import pro.sky.shop.data.ShoppingCart;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@SessionScope
+public class ShoppingCartService {
+    private final ShoppingCart shoppingCart;
+    private final DBGoods dbGoods;
+
+    public ShoppingCartService (DBGoods dbGoods) {
+        shoppingCart = new ShoppingCart();
+        this.dbGoods = dbGoods;
+    }
+
+    public Set<Integer> addGoodsToShoppingCart(List<Integer> goods) {
+        Set<Integer> adding = new HashSet<>();
+        for (int i=0; i< goods.size(); i++) {
+            if (dbGoods.getGoods().contains(goods.get(i)) && !shoppingCart.getShoppingCart().contains(goods.get(i))) {
+                shoppingCart.addToShoppingCart(goods.get(i));
+                adding.add(goods.get(i));
+            }
+        }
+        return adding;
+    }
+
+    public Set<Integer> getGoodsFromShoppingCart() {
+        return shoppingCart.getShoppingCart();
+    }
+}
