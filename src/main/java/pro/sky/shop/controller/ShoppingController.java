3103ff1bb157d62ffaaeb8316589188d98fa32b7
student <i.i.f@mail.ru>
2022-06-04 10:29:27 +0800

    all tasks

--- src/main/java/pro/sky/shop/controller/ShoppingController.java
+++ src/main/java/pro/sky/shop/controller/ShoppingController.java
@@ -1,11 +1,14 @@
 package pro.sky.shop.controller;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.shop.exception.BadRequestException;
 import pro.sky.shop.service.ShoppingCartService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -19,8 +22,16 @@ public class ShoppingController {
     }
 
     @GetMapping(path ="/add")
-    public Set<Integer> addGoodsToShoppingCart (@RequestParam("id") List <Integer> goods) {
-        return shoppingCartService.addGoodsToShoppingCart(goods);
+    public Set<Integer> addGoodsToShoppingCart (@RequestParam("id") List <String> goods) {
+        List <Integer> items = new ArrayList<>();
+        for (int i=0; i<goods.size(); i++) {
+            try {
+                items.add(Integer.parseInt(goods.get(i)));
+            } catch (RuntimeException e) {
+                throw new BadRequestException();
+            }
+        }
+        return shoppingCartService.addGoodsToShoppingCart(items);
     }
 
     @GetMapping(path ="/get")
---
+++ src/main/java/pro/sky/shop/exception/BadRequestException.java
@@ -0,0 +1,10 @@
+package pro.sky.shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    public BadRequestException() {
+    }
+}
