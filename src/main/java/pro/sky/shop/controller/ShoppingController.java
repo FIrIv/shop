dfdc9dbff7799cd4854ab5ac5b16d3685bed151f
student <i.i.f@mail.ru>
2022-06-05 22:45:44 +0800

    all tasks

--- src/main/java/pro/sky/shop/controller/ShoppingController.java
+++ src/main/java/pro/sky/shop/controller/ShoppingController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/store/order")
+@RequestMapping("/order")
 public class ShoppingController {
     private final ShoppingCartService shoppingCartService;
 
